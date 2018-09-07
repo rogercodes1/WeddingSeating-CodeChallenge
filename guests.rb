016f39b294b56b8a8d740ebebdd185a1f76a05e9
rogercodes1 <rogerperez.us@gmail.com>
2018-09-07 14:35:14 -0700

    added inputs.

--- guests.rb
+++ guests.rb
@@ -1,8 +1,28 @@
 class Guest
 
-  def initialize(argument)
-    @argument = argument
+  def initialize(guest_name, size)
+    @guest_name = guest_name
+    @size = size
   end
 
 
 end
+
+# tables: A-8 B-8 C-7 D-7
+# Thornton, party of 3
+# Garcia, party of 2
+# Owens, party of 6 dislikes Thornton, Taylor
+# Smith, party of 1 dislikes Garcia
+# Taylor, party of 5
+# Reese, party of 7
+# Example output:
+# Table A
+# Thornton, party of 3
+# Taylor, party of 5
+# Table B
+# Smith, party of 1
+# Owens, party of 6
+# Table C
+# Garcia, party of 2
+# Table D
+# Reese, party of 7
